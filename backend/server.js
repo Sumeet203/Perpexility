c2ae5f15a0b858c4c90d6ca449ea9ee4293b87b1
Sumeet203 <sumeetkothiyal203@gmail.com>
2026-05-17 09:08:34 +0530

    feat: integrate Socket.IO for real-time communication and update chat functionality

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,16 @@ import app from "./src/app.js";
 import connectDB from "./src/config/database.js";
 import { testAi } from './src/services/ai.service.js';
 const PORT = Number.parseInt(process.env.PORT, 10) || 3000;
+import http from 'http';
+import { initSocket } from './src/sockets/server.socket.js';
 // testAi();
+
+const httpServer = http.createServer(app);
+initSocket(httpServer);
 async function startServer() {
     try {
         await connectDB();
-        app.listen(PORT, () => {
+        httpServer.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         });
     } catch (err) {
--- frontend/src/features/chat/pages/Dashboard.jsx
+++ frontend/src/features/chat/pages/Dashboard.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
-
+import { useChat } from '../hooks/useChat';
 const Dashboard = () => {
     const {user} = useSelector(state=>state.auth);
     console.log("User in Dashboard:", user);
+    const chat = useChat();
+    useEffect(()=>{
+      chat.initializeSocketConnection();
+    },[]);
   return (
     <div>
       Welcome to Dashboard
